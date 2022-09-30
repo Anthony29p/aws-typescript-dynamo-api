6abc0821bca0b86b069906bd27620b06c06c9a0e
anthony29p <anthony.perez@uni.pe>
2022-09-30 15:53:28 -0500

    validations enabled

--- src/functions/postCard.ts
+++ src/functions/postCard.ts
@@ -1,18 +1,27 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import * as validations from '../validations/cardValidation';
 const { v4 } = require('uuid');
 const AWS = require('aws-sdk')
 const jwt =  require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
 const postCard: APIGatewayProxyHandler = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
 
   let { card_number,cvv,expiration_month,expiration_year,email } = JSON.parse(event.body);
-  const createdAt = new Date();
-  const id = v4()
 
+  const date = new Date();
+  let createdAt = date.toString()
+
+  const id = v4()
   const token:string =jwt.sign({id},'tokenKey',{expiresIn:15*60*1000})
 
+  let error:string = 'error at';
+  if(!validations.cardValidation(card_number)){error +=' card_number'}
+  if(!validations.cvvValidation(cvv)){error +=' cvv'}
+  if(!validations.monthValidation(expiration_month)){error +=' expiration_month'}
+  if(!validations.yearValidation(expiration_year)){error +=' expiration_year'}
+  
   const newCard = {
     id,
     card_number,
@@ -31,14 +40,23 @@ const postCard: APIGatewayProxyHandler = async (event) => {
   })
   .promise()
 
-  return{
-    statusCode: 200,
-    body: JSON.stringify(newCard),
-    headers: {
-      'auth-token': token,
-    },
+
+  if(error==='error at'){
+    return{
+      statusCode: 200,
+      body: JSON.stringify(newCard),
+      headers: {
+        'auth-token': token,
+      },
+    }
+  } 
+  else{
+    return{
+      statusCode: 400,
+      body: JSON.stringify(error),
+      
+    }
   }
-  
 }
 
 
--- src/validations/cardValidation.ts
+++ src/validations/cardValidation.ts
@@ -9,29 +9,33 @@ const luhnAlgorithm =(card_number:number) =>{
         if(i%2===0){
             num *= 2
         }
+        if(num>10){
+           num = Math.floor(num/10) +num%10
+        }
         sum += num;
     }
 
     return(sum%10===0)
 }
 
-const cardValidation = (card_number:any) => {
+export const cardValidation = (card_number:any) => {
     
-    return(Number.isInteger(card_number) && Math.pow(10,13)<card_number && card_number<Math.pow(10,17))
+    return(Number.isInteger(card_number) && Math.pow(10,13)<card_number && card_number<Math.pow(10,17) && luhnAlgorithm(card_number))
 }
 
-const cvvValidation = (cvv:any) => {
+export const cvvValidation = (cvv:any) => {
     
     return(Number.isInteger(cvv) && Math.pow(10,3)<cvv && cvv<=Math.pow(10,4))
 }
 
-const monthValidation = (month:any) => {
+export const monthValidation = (month:any) => {
     
     return(typeof month === 'string' && 0<Number(month) && Number(month)<=12)
 }
 
-const yearValidation = (year:any) => {
+export const yearValidation = (year:any) => {
     let fecha = new Date();
     let año = fecha.getFullYear()
-    return(typeof year === 'string' && año<Number(year) && Number(year)<=año+5)
+    return(typeof year === 'string' && año<=Number(year)-5 && Number(year)<=año+5)
 }
+
